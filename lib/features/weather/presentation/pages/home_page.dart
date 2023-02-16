de68ea267ac5513bc20528eefab456800f1175ce
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-02-16 17:20:57 +0100

    tolto lo stateful con la prova di chiamata api

--- lib/features/weather/presentation/pages/home_page.dart
+++ lib/features/weather/presentation/pages/home_page.dart
@@ -1,35 +1,7 @@
-import 'package:dartz/dartz.dart' as dartz;
 import 'package:flutter/material.dart';
-import 'package:openweather_cubit/core/errors/failures.dart';
-import 'package:openweather_cubit/features/weather/data/models/weather_model.dart';
-import 'package:openweather_cubit/features/weather/domain/repositories/weather_repository.dart';
 
-import '../../../../core/services/service_locator.dart';
-import '../../domain/entities/weather.dart';
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-_fetchWeatherFromCurrentLocation() async {
-  WeatherRepository repo = sl();
-  dartz.Either<Failure, Weather> response =
-      await repo.fetchWeatherFroMCurrentLocation();
-  response.fold(
-    (failure) => print(failure),
-    (weather) => print(weather),
-  );
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    super.initState();
-    _fetchWeatherFromCurrentLocation();
-  }
 
   @override
   Widget build(BuildContext context) {
